7ee3e042541376235868b0767296d171ae52699e
Samantha Allen <samanthaamiraallen@gmail.com>
2024-01-11 10:26:13 -0400

    added readme template and user prompt questions

--- index.js
+++ index.js
@@ -1,7 +1,44 @@
 // TODO: Include packages needed for this application
+const inquirer = require("inquirer");
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [{
+    type: 'input',
+    message: 'What is the title of your project?',
+    name: 'title',
+},
+{
+    type: 'input',
+    message: 'Write a brief summary describing your project: ',
+    name: 'description',   
+}, 
+{
+    type: 'input',
+    message: 'Are there any installation instructions? ',
+    name: 'installation',  
+}, 
+{
+    type: 'input',
+    message: 'Any usage information to be included? ',
+    name: 'usage',  
+},
+{
+    type: 'input',
+    message: 'How can others contribute? ',
+    name: 'contributions',  
+},
+{
+    type: 'input',
+    message: 'Here is how to test the project: ',
+    name: 'tests',  
+},
+{
+    type: 'list',
+    message: 'Which license does your project have? ',
+    name: 'license',  
+    choices: ''
+}
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
