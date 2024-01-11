59e43e328b53049f23e98c6b375fcd52d890c509
Samantha Allen <samanthaamiraallen@gmail.com>
2024-01-11 11:18:04 -0400

    added write to file function

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 // TODO: Include packages needed for this application
 const inquirer = require("inquirer");
+const fs = require('fs');
 
 // TODO: Create an array of questions for user input
 const questions = [{
@@ -72,10 +73,21 @@ const questions = [{
 ];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(data) {
+    inquirer
+        .prompt(questions)
+        .then((data) => {
+            const fileName = `${data.title.split(" ").join("")}README.md`;
+
+            fs.writeFile(fileName, JSON.stringify(data, null, "\t")), (err) =>
+            err ? console.log(err) : console.log("Success!")
+        });
+};
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    writeToFile();
+};
 
 // Function call to initialize app
 init();
