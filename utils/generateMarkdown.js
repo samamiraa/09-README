c47441714c75c298b8be4c29f65177a3b29d422c
Samantha Allen <samanthaamiraallen@gmail.com>
2024-01-12 11:06:21 -0400

    added comments to generateMarkdown.js

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,11 +1,12 @@
-// TODO: Create a function to generate markdown for README
+//function to generate markdown for README
 function generateMarkdown(data) {
-  return `# ${data.title}
+  // returns template literal
+  return `# ${data.title/*adds title inputted by user*/}
 
-  ![Static Badge](https://img.shields.io/badge/License-${data.license}-green)
+  ![Static Badge](https://img.shields.io/badge/License-${data.license/*adds license chosen by user, creates badge*/}-blue)
 
   ## Description
-  ${data.description}
+  ${data.description/*adds description inputted by user */}
   
   ## Table of Contents
   
@@ -13,41 +14,44 @@ function generateMarkdown(data) {
   - [Usage](#usage)
   - [Credits](#credits)
   - [License](#license)
-  - [Features](#Features)
-  - [Questions](#Questions)
-  - [Contributions](#Contributions)
-  - [Tests](#Tests)
+  - [Features](#features)
+  - [Questions](#questions)
+  - [Contributions](#contributions)
+  - [Tests](#tests)
   
   ## Installation
-  ${data.installation}
+  ${data.installation/*adds installation inputted by user  */}
   
   ## Usage
-  ${data.usage}
+  ${data.usage/*adds usage inputted by user  */}
   
   ## Credits
-  ${data.credits}
+  ${data.credits/*adds credits inputted by user  */}
   
   ## License
-  ![Static Badge](https://img.shields.io/badge/License-${data.license}-blue)
-  This application is covered under the ${data.license}.
+  ![Static Badge](https://img.shields.io/badge/License-${data.license /*adds license chosen by user, creates badge*/}-blue) <br>
+  <br>
+
+  This application is covered under ${data.license/*adds license chosen by user*/} license.
   
   ## Features
-  ${data.features}
+  ${data.features/*adds features inputted by user*/}
 
   ## Questions
   Have questions?
 
-  Find me on Github: <a href"https://github.com/${data.username}">${data.username}</a>
+  Find me on Github: https://github.com/${data.username/*adds github username inputted by user*/}
 
-  Email me at: ${data.email}
+  Email me at: ${data.email/*adds email inputted by user*/}
   
-  ## How to Contribute
-  ${data.contributions}
+  ## Contributions
+  ${data.contributions/*adds contributions inputted by user*/}
   
   ## Tests
-  ${data.tests}
+  ${data.tests/*adds tests inputted by user*/}
   
 `;
 }
 
+// exports generateMarkdown
 module.exports = generateMarkdown;
